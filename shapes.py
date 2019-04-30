334cb381ec78ea6c637f8dc8dc59d627b0ec1bcb
naymayakub <47854378+naymayakub@users.noreply.github.com>
2019-04-30 19:31:31 +0300

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,68 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+
+    def area(a,b):
+        area = 3.14*a*b
+        print(area)
+
+    def circumference(a):
+        circumference = 2*3.14*a
+        print(circumference)
+
+
+   class Square:
+    def __init__(self,a):
+        self.a = a
+
+    def squares(a):
+        a = a*a
+        print(a)
+
+    def perimeter(a):
+        a = 4*a
+        print(a)
+
+    class Rectangles:
+        def __init__(self,a):
+        self.a = a
+
+    def A(w,l):
+        A = w*l
+        print(A)
+
+    class sphere:
+        def __init__(self,a):
+        self.a = a
+
+    def SA(a):
+        a = 4*3.14*a*a
+        print(a)
+
+    def V(a):
+        a = 4/3*3.14*a*a*a
+        print(a)
+        
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
