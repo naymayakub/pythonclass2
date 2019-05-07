2497cec0c6e4e231277cba10da51b67eb195c421
naymayakub <47854378+naymayakub@users.noreply.github.com>
2019-05-07 19:18:24 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,54 @@
+tens=range(1,9)
+for x in tens:
+    print(x)
+a=[x*10 for x in tens]
+
+
+
+z=[2,4,6,8,10,12]
+y=[x*x for x in z]
+
+
+sorted[[9,1,4,7,3],[5,2,6,8,0]]
+m=[]
+for x in sorted:
+    for z in x
+    m.append(z)
+
+
+def range_sum(1,n)
+    answer=1+n
+    return answer
+
+
+max(1,3,6,8,2,4,5.7)
+
+
+min(1,3,6,8,2,4,5.7)
+
+
+
+for x in range(10,20):
+    if x % 3==0:
+        print("{} is divisible by 3").
+        (format(x))
+
+
+def hello ('balance':1000, 'name':'Irene')
+print('hello {}, your balance is {}'.format 'name', 'balance')
+
+def hello ("balance":2000, "name": "Pauline")
+print('hello {}, your balance is {}'.format 'name', 'balance')
+
+def hello ("balance":3000, "name": "Naima")
+print('hello {}, your balance is {}'.format 'name', 'balance')
+
+def hello ("balance":1000, "name": "Rose")
+print('hello {}, your balance is {}'.format 'name', 'balance')
+
+
+
+
+
+
+
